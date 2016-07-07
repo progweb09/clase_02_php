95f3fcce82d9706991b97a4a4272f58a861f433f
julindres <tengo3cmdepito@gmail.com>
2016-07-07 20:05:07 -0300

    ijii

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Show actual date time server on Spanish...
+ * @author Andres Plehm y Julio coria
+ * @copyright
+ * @version 1.0.1b
+ * 
+ * 
+ * 
+ */
+
+define($name, $value)
+
+$horaActual=date("H:i:s");
+ echo "<h1 style='color: navy;'>La hora actual es: $horaActual</h1>";
+
+ 
+ 
+ 
+ 
+ 
+?>
